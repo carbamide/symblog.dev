40b60ab68252df1db7fea600bf5c990ecd5b82d8
Josh Barrow <josh@jukaela.com>
2013-01-11 17:00:44 -0600

    Begin contact view

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -3,6 +3,9 @@ namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+
 class PageController extends Controller
 {
 	public function indexAction()
@@ -13,5 +16,25 @@ class PageController extends Controller
 	public function aboutAction()
 	{
 		return $this->render('BloggerBlogBundle:Page:about.html.twig');
+	}
+	
+	public function contactAction()
+	{
+		$enquiry = new Enquiry();
+		$form = $this->createForm(new EnquiryType(), $enquiry);
+		
+		$request = $this->getRequest();
+		
+		if ($request->getMethod() == 'POST') {
+			$form->bindRequest($request);
+			
+			if ($form->isValid()) {
+				//Do stuff!
+				
+				return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+			}
+		}
+		
+		return $this->render('BloggerBlogBundle:Page:contact.html.twig', array('form' => $form->createView()));
 	}
 }
