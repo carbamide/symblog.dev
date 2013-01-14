bb5e32d616315918dff764cd847424f6edeafcd7
Josh Barrow <josh@jukaela.com>
2013-01-14 09:06:00 -0600

    Begin setting up validation for contact page and emailer

--- src/Blogger/BlogBundle/Entity/Enquiry.php
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -2,6 +2,12 @@
 
 namespace Blogger\BlogBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\MinLength;	
+use Symfony\Component\Validator\Constraints\MaxLength;
+	
 class Enquiry
 {
 	protected $name;
@@ -47,5 +53,14 @@ class Enquiry
 	public function setBody($body)
 	{
 		$this->body = $body;
+	}
+	
+	public static function loadValidatorMetadata(ClassMetadata $metadata) 
+	{
+		$metadata->addPropertyConstraint('name', new NotBlank());
+		$metadata->addPropertyConstraint('email', new Email(array('message' => 'symblog does not like invalid emails!  Give me a real one!')));
+		$metadata->addPropertyConstraint('subject', new NotBlank());
+		$metadata->addPropertyConstraint('subject', new MaxLength(50));
+		$metadata->addPropertyConstraint('body', new MinLength(50));
 	}
 }
