c3c73727e2e01b830098ba19e6edfa2b8e853f10
Josh Barrow <josh@jukaela.com>
2013-01-14 11:57:00 -0600

    Add comments repository and fixture for loading comments

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,12 +8,14 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
+	        new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+	    	new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\AopBundle\JMSAopBundle(),
--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -10,7 +10,11 @@ class PageController extends Controller
 {
 	public function indexAction()
 	{
-		return $this->render('BloggerBlogBundle:Page:index.html.twig');
+		$em = $this->getDoctrine()->getEntityManager();
+		
+		$blogs = $em->getRepository('BloggerBlogBundle:Blog')->getLatestBlogs();
+		
+		return $this->render('BloggerBlogBundle:page:index.html.twig', array('blogs' => $blogs));
 	}
 	
 	public function aboutAction()
